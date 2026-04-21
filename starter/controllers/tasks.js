26705432e885d0a56c9aa883c3f5b8480addeeb0
Ann Kariuki <awk19@duke.edu>
2026-04-20 21:26:51 -0700

    initial model and schema setup

--- starter/controllers/tasks.js
+++ starter/controllers/tasks.js
@@ -1,10 +1,13 @@
+const Task = require('../models/Task')
+
 const getAllTasks = (req, res) => {
     res.send('get all tasks');
 };
 
-const createTask = (req,res) => {
-    console.log(req.body)
-    res.send(req.body);
+const createTask = async (req,res) => {
+    // console.log(req.body)
+    const task = await Task.create(req.body)
+    res.status(201).json({task})
 };
 const getTask = (req,res) => {
     res.send({id:req.params.id});
